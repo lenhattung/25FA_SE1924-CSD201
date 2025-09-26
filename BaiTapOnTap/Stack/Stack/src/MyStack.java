67478b025287cce2ba10bb43eb64c15dde3e3186
lenhattung <lenhattung@gmail.com>
2025-09-26 11:20:16 +0700

    u
    u

--- BaiTapOnTap/Stack/Stack/src/MyStack.java
+++ BaiTapOnTap/Stack/Stack/src/MyStack.java
@@ -1,17 +1,24 @@
+
 import java.util.*;
 import java.io.*;
 
 class MyStack {
+
     Node top;
+
     // Constructor
-    MyStack() { this.top = null; }
- 
+    MyStack() {
+        this.top = null;
+    }
+
     public void push(int x) // insert at the beginning
     {
-		
+        Node newNode = new Node(x);
+        newNode.setNext(top);
+        top = newNode;
     }
-    
-     public void load() // insert at the beginning
+
+    public void load() // insert at the beginning
     {
         this.push(10);
         this.push(14);
@@ -22,32 +29,40 @@ class MyStack {
         this.push(15);
         this.push(9);
     }
- 
+
     // Utility function to check if the stack is empty or
     // not
-    public boolean isEmpty() { return top == null; }
- 
+    public boolean isEmpty() {
+        return top == null;
+    }
+
     // Utility function to return top element in a stack
-    public int peek()
-    {
-        
-		
-		
+    public int peek() {
+        if (isEmpty()) {
+            return 0;
+        } else {
+            return top.getInfo();
+        }
     }
- 
+
     // Utility function to pop top element from the stack
-    public void pop() // remove at the beginning
+    public int pop() // remove at the beginning
     {
-        
-		
-		
+        if (isEmpty()) {
+            return 0;
+        } else {
+            int temp = top.getInfo();
+            top = top.getNext();
+            return temp;
+        }
     }
- 
-    public void display()
-    {
-        
-		
-		
+
+    public void display() {
+        Node temp = top;
+        while(temp!=null){
+            System.out.print(temp.getInfo()+" ");
+            temp = temp.getNext();
+        }
     }
-   
+
 }
--- BaiTapOnTap/Stack/Stack/src/Node.java
+++ BaiTapOnTap/Stack/Stack/src/Node.java
@@ -13,4 +13,22 @@ class Node {
     Node(int x) {
         this(x, null);
     }
+
+    public int getInfo() {
+        return info;
+    }
+
+    public void setInfo(int info) {
+        this.info = info;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    
+    
 }
