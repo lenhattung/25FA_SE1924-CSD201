311f2f04f8ee608562370f9e13f40ad50266ebec
lenhattung <lenhattung@gmail.com>
2025-09-26 11:34:44 +0700

    u
    u

--- BaiTapOnTap/Queue/Queue/src/MyQueue.java
+++ BaiTapOnTap/Queue/Queue/src/MyQueue.java
@@ -1,34 +1,41 @@
+
 import java.util.*;
 import java.io.*;
 
 class MyQueue {
+
     Node head, tail;
- 
-    public MyQueue() 
-	{ 
-		this.head = this.tail = null; 
-	}
- 
+
+    public MyQueue() {
+        this.head = this.tail = null;
+    }
+
     // Method to add an key to the queue.
-    void enqueue(int key)
-    {
- 
- 
- 
- 
+    void enqueue(int key) {
+        Node newNode = new Node(key);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
     }
- 
+
     // Method to remove an key from queue.
-    void dequeue()
-    {
-        
-		
-		
-		
-		
-		
+    int dequeue() {
+        if (isEmpty()) {
+            return 0;
+        } else {
+            int temp = head.getInfo();
+            head = head.getNext();
+            if (head == null) {
+                tail = null;
+            }
+            return temp;
+        }
     }
-     public void load() // insert at the beginning
+
+    public void load() // insert at the beginning
     {
         this.enqueue(10);
         this.enqueue(14);
@@ -39,37 +46,33 @@ class MyQueue {
         this.enqueue(15);
         this.enqueue(9);
     }
- 
+
     // Utility function to check if the stack is empty or
     // not
-    public boolean isEmpty() 
-	{ 
+    public boolean isEmpty() {
+        return head == null && tail == null;
+    }
 
-	}
- 
     // Utility function to return top element in a stack
-    public int front()
-    {
-		
-		
-		
+    public int front() {
+        if (isEmpty()) {
+            return 0;
+        } else {
+            int temp = head.getInfo();
+            return temp;
+        }
     }
-   
- 
-    public void display()
-    {
-        
-		
-		
-		
+
+    public void display() {
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.getInfo()+" ");
+            temp = temp.getNext();
+        }
     }
-	
-	void enqueueWithPriority(int key, int priority_level)
-    {
- 
- 
- 
- 
+
+    void enqueueWithPriority(int key, int priority_level) {
+
     }
-   
+
 }
