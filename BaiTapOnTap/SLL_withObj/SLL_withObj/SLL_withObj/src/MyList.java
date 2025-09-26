fa8f8ee5f9a9bd0cba85aae418ff9554b880f93f
lenhattung <lenhattung@gmail.com>
2025-09-26 11:02:01 +0700

    u
    u

--- BaiTapOnTap/SLL_withObj/SLL_withObj/SLL_withObj/src/MyList.java
+++ BaiTapOnTap/SLL_withObj/SLL_withObj/SLL_withObj/src/MyList.java
@@ -1,162 +1,228 @@
+
 import java.util.*;
 import java.io.*;
 
 public class MyList {
+
     Node head, tail;
     int size;
 
     MyList() {
         this.head = null;
-        this.tail=null;
+        this.tail = null;
         this.size = 0;
     }
-    
+
     public boolean isEmpty() {
         return this.size == 0;
     }
-    
+
     public void clear() {
         this.head = null;
         this.tail = null;
         this.size = 0;
     }
-    
+
     void ftraverse(RandomAccessFile f) throws Exception {
         Node p = head;
-        while(p != null) {
-            f.writeBytes(p.info.name + "-" + p.info.price + "     "); // write data in the node p to the file f
-            p = p.next;
+        while (p != null) {
+            f.writeBytes(p.getInfo().getName() + "-" + p.getInfo().getPrice() + "     "); // write data in the node p to the file f
+            p = p.getNext();
         }
-        
+
         f.writeBytes("\r\n");
     }
-    
+
     void loadData(int k) {
-        String [] a = Lib.readLineToStrArray("data.txt", k);
-        String [] b = Lib.readLineToStrArray("data.txt", k+1);
+        String[] a = Lib.readLineToStrArray("data.txt", k);
+        String[] b = Lib.readLineToStrArray("data.txt", k + 1);
         int n = a.length;
-        for(int i = 0; i < n; i++) 
-        {
+        for (int i = 0; i < n; i++) {
             int p = Integer.parseInt(b[i]);
-            
+
             addLast(a[i], p);
         }
     }
-     
+
     void addLast(String n, int p) {
         //------------------------------------------------------------------------------------
         //------ Start your code here---------------------------------------------------------
-        Phone data = new Phone(n,p);
+        Phone data = new Phone(n, p);
         Node tmp_Node = new Node(data);
 
-           
-       if (head == null)
-       {
-           head = tail = tmp_Node;
-       }
-       else
-       {
-           tail.next = tmp_Node;
-           tail = tail.next;
-       }
-       size++;
-        
+        if (head == null) {
+            head = tail = tmp_Node;
+        } else {
+            tail.setNext(tmp_Node);
+            tail = tail.getNext();
+        }
+        size++;
+
         //------ End your code here-----------------------------------------------------------
         //------------------------------------------------------------------------------------
     }
-    
-	// f1: ham nay se goi ham addLast nhieu lan
+
+    // f1: ham nay se goi ham addLast nhieu lan
     void f1() throws Exception {
         clear();
         loadData(0);
         String fname = "f1.txt";
         File g123 = new File(fname);
-        if(g123.exists()) g123.delete();
-        RandomAccessFile f = new RandomAccessFile(fname, "rw"); 
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
         ftraverse(f);
         f.close();
     }
+
     
-	// f2: ham addFirst ==> du lieu nhap tu ban phim
+    void addFirst(String n, int p) {
+        //------------------------------------------------------------------------------------
+        //------ Start your code here---------------------------------------------------------
+        Phone data = new Phone(n, p);
+        Node tmp_Node = new Node(data);
+
+        if (head == null) {
+            head = tail = tmp_Node;
+        } else {
+            tmp_Node.setNext(head);
+            head = tmp_Node;
+        }
+        size++;
+
+        //------ End your code here-----------------------------------------------------------
+        //------------------------------------------------------------------------------------
+    }
+    // f2: ham addFirst ==> du lieu nhap tu ban phim
     void f2() throws Exception {
         clear();
         loadData(0);
         String fname = "f2.txt";
         File g123 = new File(fname);
-        if(g123.exists()) g123.delete();
-        RandomAccessFile  f = new RandomAccessFile(fname, "rw"); 
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
         ftraverse(f);
         //------------------------------------------------------------------------------------
         //------ Start your code here---------------------------------------------------------
-
-       
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input name: ");
+        String name = sc.nextLine();
+        System.out.println("Input price: ");
+        int price = sc.nextInt();
+        
+        addFirst(name, price);
         //------ End your code here-----------------------------------------------------------
         //------------------------------------------------------------------------------------
         ftraverse(f);
         f.close();
     }
-    
+
     // f3: ham computeSum ==> tinh tong gia tri cua tat ca cac Phone co trong list
     void f3() throws Exception {
         clear();
         loadData(0);
         String fname = "f3.txt";
         File g123 = new File(fname);
-        if(g123.exists()) g123.delete();
-        RandomAccessFile  f = new RandomAccessFile(fname, "rw"); 
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
         ftraverse(f);
         //------------------------------------------------------------------------------------
         //------ Start your code here---------------------------------------------------------
-
-        
-
+        Node temp = head;
+        int computeSum = 0;
+        while(temp!=null){
+            computeSum+=temp.getInfo().getPrice();
+            temp = temp.getNext();
+        }
+        System.out.println(computeSum);
         //------ End your code here-----------------------------------------------------------
         //------------------------------------------------------------------------------------
         ftraverse(f);
         f.close();
     }
+
+    void removePos(int k){
+        if(isEmpty()){
+            return;
+        }
+        Node beforeDeleteNode = head;
+        // Tim vi tri truoc node can xoa
+        for (int i = 0; i < k-1; i++) {
+            if (beforeDeleteNode != null) {
+                beforeDeleteNode = beforeDeleteNode.getNext();
+            }
+        }
+        // Xoa di q
+        // p -> q -> null => p->null
+        // p -> q -> r -> null => p->r->null
+        if(beforeDeleteNode!=null){
+            beforeDeleteNode.setNext(beforeDeleteNode.getNext().getNext());
+            size--;
+        }
+    }
     
-	// f4: remove the most expensive Phone
+    // f4: remove the most expensive Phone
     void f4() throws Exception {
         clear();
         loadData(0);
         String fname = "f4.txt";
         File g123 = new File(fname);
-        if(g123.exists()) g123.delete();
-        RandomAccessFile f = new RandomAccessFile(fname, "rw"); 
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
         ftraverse(f);
         //------------------------------------------------------------------------------------
         //------ Start your code here---------------------------------------------------------
-        
-       
-
+        int maxPrice = Integer.MIN_VALUE;
+        int maxPosition = -1;
+        Node temp = head;
+        int i=0;
+        while(temp!=null){
+            if(temp.getInfo().getPrice()>maxPrice){
+                maxPrice = temp.getInfo().getPrice();
+                maxPosition = i;
+            }
+            temp=temp.getNext();
+            i++;
+        }
+        removePos(maxPosition);
         //------ End your code here-----------------------------------------------------------
         //------------------------------------------------------------------------------------
         ftraverse(f);
         f.close();
     }
-	
-	// f5: discount all Phone 'S' with 10%
-	 void f5() throws Exception {
+
+    // f5: discount all Phone 'S' with 10%
+    void f5() throws Exception {
         clear();
         loadData(0);
         String fname = "f5.txt";
         File g123 = new File(fname);
-        if(g123.exists()) g123.delete();
-        RandomAccessFile  f = new RandomAccessFile(fname, "rw"); 
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
         ftraverse(f);
         //------------------------------------------------------------------------------------
         //------ Start your code here---------------------------------------------------------
-
-       
-
+        Node temp = head;
+        while(temp!=null){
+            if(temp.getInfo().getName().equals("S")){
+                int price =temp.getInfo().getPrice();
+                temp.getInfo().setPrice((int)(price*0.9));
+            }
+            temp = temp.getNext();
+        }
         //------ End your code here-----------------------------------------------------------
         //------------------------------------------------------------------------------------
         ftraverse(f);
         f.close();
     }
-    
-  
-}
 
+}
--- BaiTapOnTap/SLL_withObj/SLL_withObj/SLL_withObj/src/Node.java
+++ BaiTapOnTap/SLL_withObj/SLL_withObj/SLL_withObj/src/Node.java
@@ -1,23 +1,66 @@
-class Node{
-    Phone info;
-    Node next;
-    Node () {}
-    Node (Phone x, Node p) {
+
+public class Node {
+
+    private Phone info;
+    private Node next;
+
+    Node() {
+    }
+
+    Node(Phone x, Node p) {
         this.info = x; // data stored inside the node
         this.next = p; // link to the next node
     }
-    Node (Phone x) {
-        this(x,null);
-    }
- }
 
-class Phone{
-    String name;
-    int price;
-    Phone () {}
-    Phone (String n, int p) {
+    Node(Phone x) {
+        this(x, null);
+    }
+
+    public Phone getInfo() {
+        return info;
+    }
+
+    public void setInfo(Phone info) {
+        this.info = info;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+    
+
+}
+
+class Phone {
+    private String name;
+    private int price;
+
+    Phone() {
+    }
+
+    Phone(String n, int p) {
         this.name = n; // data stored inside the node
         this.price = p; // link to the next node
     }
- }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
